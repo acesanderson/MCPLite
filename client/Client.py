e01c949e5cf811b325588b4480677d3c6fe8776e
bianders <phat@biden.co.uk>
2025-04-26 22:28:29 -0500

    cleaned things up; host directory; e2e.py script in main is where I'll test the entire chain

--- client/Client.py
+++ client/Client.py
@@ -1,4 +1,4 @@
-from MCPLite.messages.MCPMessage import MCPMessage
+from MCPLite.messages.Requests import MCPRequest
 from MCPLite.primitives.MCPRegistry import ClientRegistry
 from MCPLite.transport.Transport import Transport, DirectTransport
 from MCPLite.server.Server import Server
@@ -21,27 +21,22 @@ class Client:
         # Validate that we've received JSON, and that the JSON matches the MCP schema for list_definitions.
         # Create tool, resource, prompt objects from the JSON received from the server.
         # Add the tools, resources, prompts to the registry.
-        self.registry = (
-            self.server.registry
-        )  # purpose: this mocks network calll; we call this 'DirectClientTransport'
+        # self.registry = (
+        #     self.server.registry
+        # )  # purpose: this mocks network calll; we call this 'DirectClientTransport'
         pass
 
-    def send_request(self, request: MCPMessage) -> Optional[MCPMessage]:
+    def send_request(self, request: MCPRequest) -> Optional[MCPRequest]:
         """
         Send a request to the server.
         """
         # Validate pydantic object matches the client/server schema.
         # Convert to JSON.
-        json_str = request.model_dump_json()
-        # Send to the server.
-        # If the server responds, parse the response and return it to the client as pydantic object.
-        server_response_in_json = self.server.return_answer(json_str)
-        # Convert to pydantic object.
-        server_response_dict = json.loads(server_response_in_json)
-        server_response_pydantic = ToolResponse(**server_response_dict)
-        return server_response_pydantic
+        jsonrpc_request = request.to_jsonrpc()
+        json_str = jsonrpc_request.model_dump_json()
+        self.transport.send_json(json_str)
 
 
 if __name__ == "__main__":
     server = Server()
-    transport = DirectTransport()
+    transport = DirectTransport(server.process_message)
--- notes.md
+++
@@ -1,8 +0,0 @@
-# Roadmap for implementation
-## Server
-- rework the primitives (Tool, Resource, Prompt) as decorators that convert functions to mcp objects
-- these mcp objects have the following input/output/variables per the official MCP schema:
-    - definition (currently 'to_dict' -- should return json)
-    - request (this is json)
-    - response (this is the return type -- json)
-- pydantic classes for all of the above
